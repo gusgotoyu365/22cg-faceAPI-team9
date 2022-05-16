00e94011dbc228703d5365c794568f7c1b946690
Gusgotoyu365 <gusgotoyu365@naver.com>
2022-05-16 23:23:58 +0900

    눈썹과 입술 배열을 draw로 복사

--- faceAPI 과제 코드/sketch_220516ml5.js
+++ faceAPI 과제 코드/sketch_220516ml5.js
@@ -1,11 +1,19 @@
 let img, parts;
 let options = {withLandmarks: true, withDescriptors: false};
+let mouth = [], eyebrowL = [], eyebrowR = [];
 
 function preload() {
   img = loadImage('face.png');
 }
 
 function setup() {
+  for(let i=0; i<20; i++) {
+    mouth[i] = [];
+  }
+  for(let i=0; i<6; i++) {
+    eyebrowL[i] = [];
+    eyebrowR[i] = [];
+  }
   createCanvas(img.width*2, img.height);
   faceapi = ml5.faceApi(options, modelReady);
   background(255);
@@ -33,37 +41,13 @@ function gotResults(err, results) {
 function drawFace() {
   //translate(img.width,0);
   /**
-    입술그리기
+    입술배열저장
   */
-  // 입술 위쪽
-  push();
-  noStroke();
-  fill(255,100,100);
-  beginShape();
-  for(let i=0; i<7; i++){
-    vertex(parts.mouth[i]._x, parts.mouth[i]._y);
+  for(let i=0; i<parts.mouth.length; i++) {
+    mouth[i][0] = parts.mouth[i]._x;
+    mouth[i][1] = parts.mouth[i]._y;
   }
-  for(let i=16; i>11; i--) {
-    vertex(parts.mouth[i]._x, parts.mouth[i]._y);
-  }
-  vertex(parts.mouth[0]._x, parts.mouth[0]._y);
-  endShape();
-  pop();
-  //입술 아래쪽
-  push();
-  beginShape();
-  noStroke();
-  fill(255,200,200);
-  for(let i=6; i<12; i++){
-    vertex(parts.mouth[i]._x, parts.mouth[i]._y);
-  }
-  vertex(parts.mouth[0]._x, parts.mouth[0]._y);
-  for(let i=19; i>16; i--){
-    vertex(parts.mouth[i]._x, parts.mouth[i]._y);
-  }
-  vertex(parts.mouth[6]._x, parts.mouth[6]._y);
-  endShape();
-  pop();
+  
   stroke(200);
   /*strokeWeight(1);
   textSize(10);
@@ -103,25 +87,66 @@ function drawFace() {
   }
   vertex(parts.rightEye[0]._x, parts.rightEye[0]._y);
   endShape();
-
-  
-  //눈썹 그리기    
-  noFill();
-  stroke(200);
-  beginShape();
+ 
+  //눈썹배열저장
   for(let i=0; i<parts.leftEyeBrow.length; i++){
-    vertex(parts.leftEyeBrow[i]._x, parts.leftEyeBrow[i]._y);
+    eyebrowL[i][0] = parts.leftEyeBrow[i]._x;
+    eyebrowL[i][1] = parts.leftEyeBrow[i]._y;
   }
-  endShape();
-
-  beginShape();
   for(let i=0; i<parts.rightEyeBrow.length; i++){
-    vertex(parts.rightEyeBrow[i]._x, parts.rightEyeBrow[i]._y);
+    eyebrowR[i][0] = parts.rightEyeBrow[i]._x;
+    eyebrowR[i][1] = parts.rightEyeBrow[i]._y;
   }
-  endShape();
   
 }
 
-function draw() {  
+function draw() {
+  console.log(mouth[0][0]);
   //blend(img,0,0,54,19,100,156,54,19,DARKEST);
+  // 입술 위쪽
+  push();
+  noStroke();
+  fill(mouseY,100,100);
+  beginShape();
+  for(let i=0; i<7; i++){
+    vertex(mouth[i][0], mouth[i][1]);
+  }
+  for(let i=16; i>11; i--) {
+    vertex(mouth[i][0], mouth[i][1]);
+  }
+  vertex(mouth[0][0], mouth[0][1]);
+  endShape();
+  pop();
+  //입술 아래쪽
+  push();
+  beginShape();
+  noStroke();
+  fill(mouseY,200,200);
+  for(let i=6; i<12; i++){
+    vertex(mouth[i][0], mouth[i][1]);
+  }
+  vertex(mouth[0][0], mouth[0][1]);
+  for(let i=19; i>16; i--){
+    vertex(mouth[i][0], mouth[i][1]);
+  }
+  vertex(mouth[6][0], mouth[6][1]);
+  endShape();
+  pop();
+  
+  /**
+    눈썹 그리기
+  */
+  noFill();
+  stroke(200);
+  beginShape();
+  for(let i=0; i<5; i++){
+    vertex(eyebrowL[i][0], eyebrowL[i][1]);
+  }
+  endShape();
+
+  beginShape();
+  for(let i=0; i<5; i++){
+    vertex(eyebrowR[i][0], eyebrowR[i][1]);
+  }
+  endShape();
 }
