e7a2a1f84e0b50a5fddbe343e500e1b71de7c712
Gusgotoyu365 <gusgotoyu365@naver.com>
2022-05-17 13:24:46 +0900

    파일 정리 및 코드 정리

--- faceAPI 과제 코드/sketch_220516ml5.js
+++ faceAPI 과제 코드/sketch_220516ml5.js
@@ -1,24 +1,44 @@
-let img, parts;
+let img, parts, button, alphaG = 0;
 let options = {withLandmarks: true, withDescriptors: false};
-let mouth = [], eyebrowL = [], eyebrowR = [];
+let mouth = [], eyebrowL = [], eyebrowR = [], eyeL = [], eyeR = [], nose = [];
+let slider;
 
 function preload() {
   img = loadImage('face.png');
+  imgG = loadImage('Gface.png');
 }
 
 function setup() {
   for(let i=0; i<20; i++) {
     mouth[i] = [];
   }
-  for(let i=0; i<6; i++) {
+  for(let i=0; i<5; i++) {
     eyebrowL[i] = [];
     eyebrowR[i] = [];
   }
-  createCanvas(img.width*2, img.height);
+  for(let i=0; i<6; i++) {
+    eyeL[i] = [];
+    eyeR[i] = [];
+  }
+  for(let i=0; i<10; i++) {
+    nose[i] = [];
+  }
+  
+  createCanvas(img.width*2, img.height+55);
   faceapi = ml5.faceApi(options, modelReady);
   background(255);
   image(img, 0, 0);
-  //image(img, img.width, 0);    
+  picker1 = createColorPicker('#FF6464');
+  picker2 = createColorPicker('#FFC8C8');
+  picker1.position(90,height-55);
+  picker2.position(90,height-27);
+  textSize(18); 
+  button = createButton('황금비율 확인하기');
+  button.position(img.width*2-130, img.height+5);
+  button.mousePressed(goldgo);
+  img.mask(imgG);
+  slider = createSlider(0,255,127);
+  slider.position(img.width*2-133,height-25);
 }
 
 function modelReady() {
@@ -39,55 +59,6 @@ function gotResults(err, results) {
 }
 
 function drawFace() {
-  //translate(img.width,0);
-  /**
-    입술배열저장
-  */
-  for(let i=0; i<parts.mouth.length; i++) {
-    mouth[i][0] = parts.mouth[i]._x;
-    mouth[i][1] = parts.mouth[i]._y;
-  }
-  
-  stroke(200);
-  /*strokeWeight(1);
-  textSize(10);
-  for(let i=0; i<mleng; i++) {
-    if (i==19) {
-      push();
-      fill(255);
-      ellipse(10,10,100);
-      pop();
-    }
-    text(i,parts.mouth[i]._x, parts.mouth[i]._y);
-  }
-  strokeWeight(3);*/
-  /**
-    코 그리기
-  */
-  //stroke(255, 0, 255);
-  noFill();
-  beginShape();
-  for(let i=0; i<parts.nose.length; i++){
-    vertex(parts.nose[i]._x, parts.nose[i]._y);
-  }
-  endShape();
-  
-  //눈 그리기
-  fill(100);
-  beginShape();
-  for(let i=0; i<parts.leftEye.length; i++){
-    vertex(parts.leftEye[i]._x, parts.leftEye[i]._y);
-  }
-  vertex(parts.leftEye[0]._x, parts.leftEye[0]._y);
-  endShape();
-
-  beginShape();
-  for(let i=0; i<parts.rightEye.length; i++){
-    vertex(parts.rightEye[i]._x, parts.rightEye[i]._y);
-  }
-  vertex(parts.rightEye[0]._x, parts.rightEye[0]._y);
-  endShape();
- 
   //눈썹배열저장
   for(let i=0; i<parts.leftEyeBrow.length; i++){
     eyebrowL[i][0] = parts.leftEyeBrow[i]._x;
@@ -98,15 +69,116 @@ function drawFace() {
     eyebrowR[i][1] = parts.rightEyeBrow[i]._y;
   }
   
+   //눈배열저장
+  for(let i=0; i<parts.leftEye.length; i++){
+    eyeL[i][0] = parts.leftEye[i]._x;
+    eyeL[i][1] = parts.leftEye[i]._y;
+  }
+  for(let i=0; i<parts.rightEye.length; i++){
+    eyeR[i][0] = parts.rightEye[i]._x;
+    eyeR[i][1] = parts.rightEye[i]._y;
+  }
+  
+  //코배열저장
+  for(let i=0; i<parts.nose.length; i++){
+    nose[i][0] = parts.nose[i]._x;
+    nose[i][1] = parts.nose[i]._y;
+  }
+  
+  //입술배열저장
+  for(let i=0; i<parts.mouth.length; i++) {
+    mouth[i][0] = parts.mouth[i]._x;
+    mouth[i][1] = parts.mouth[i]._y;
+  }
 }
 
-function draw() {
-  console.log(mouth[0][0]);
-  //blend(img,0,0,54,19,100,156,54,19,DARKEST);
+function draw() { 
+  drawFaceinScreen(200,0);
+  //글자 만들기
+  noStroke();
+  fill(0);
+  rect(0,img.height,img.width*2,img.height+55);
+  fill(255);
+  text('윗 입술',0,height-32);
+  text('아래 입술',0,height-7);
+  text('투명도',img.width*2-200,height-7);
+  translate(img.width,0);
+  fill(255);
+  rect(0, 0, img.width, img.height);
+  drawFaceinScreen(0,1);
+  tint(230, alphaG);
+  image(imgG, 42,0,img.width-80, img.height-59);
+}
+
+// 황금비율 추가
+function goldgo() {
+  if (alphaG == 0) {
+    alphaG = 80;
+  } else if (alphaG == 80) {
+    alphaG = 0;
+  }
+  //image(imgG, 42,0,img.width-80, img.height-59);
+}
+
+function drawFaceinScreen(color,s) {
+  //눈썹 그리기
+  if (s==0) {
+    stroke(0,slider.value());
+  } else {
+    //stroke(0);
+    stroke(0,slider.value());
+  }
+  noFill();
+  beginShape();
+  
+  for(let i=0; i<5; i++){
+    vertex(eyebrowL[i][0], eyebrowL[i][1]);
+  }
+  endShape();
+  
+  beginShape();
+  for(let i=0; i<5; i++){
+    vertex(eyebrowR[i][0], eyebrowR[i][1]);
+  }
+  endShape();
+
+  //눈 그리기
+  push();
+  if (s==0) {
+    fill(200);
+    stroke(0);
+  }
+  beginShape();
+  for(let i=0; i<5; i++){
+    vertex(eyeL[i][0], eyeL[i][1]);
+  }
+  vertex(eyeL[0][0], eyeL[0][1]);
+  endShape();
+
+  beginShape();
+  for(let i=0; i<5; i++){
+    vertex(eyeR[i][0], eyeR[i][1]);
+  }
+  vertex(eyeR[0][0], eyeR[0][1]);
+  endShape();
+  pop();
+  
+  //코 그리기
+  noFill();
+  beginShape();
+  for(let i=0; i<10; i++){
+    vertex(nose[i][0], nose[i][1]);
+  }
+  endShape();
+  
   // 입술 위쪽
   push();
   noStroke();
-  fill(mouseY,100,100);
+  if (s==0) {
+    fill(picker1.color(),picker1.color(),picker1.color());
+  } else {
+    stroke(0,slider.value());
+  }
   beginShape();
   for(let i=0; i<7; i++){
     vertex(mouth[i][0], mouth[i][1]);
@@ -117,11 +189,16 @@ function draw() {
   vertex(mouth[0][0], mouth[0][1]);
   endShape();
   pop();
+  
   //입술 아래쪽
   push();
   beginShape();
   noStroke();
-  fill(mouseY,200,200);
+  if (s==0) {
+    fill(picker2.color(),picker2.color(),picker2.color());
+  } else {
+    stroke(0,slider.value());
+  }
   for(let i=6; i<12; i++){
     vertex(mouth[i][0], mouth[i][1]);
   }
@@ -132,21 +209,4 @@ function draw() {
   vertex(mouth[6][0], mouth[6][1]);
   endShape();
   pop();
-  
-  /**
-    눈썹 그리기
-  */
-  noFill();
-  stroke(200);
-  beginShape();
-  for(let i=0; i<5; i++){
-    vertex(eyebrowL[i][0], eyebrowL[i][1]);
-  }
-  endShape();
-
-  beginShape();
-  for(let i=0; i<5; i++){
-    vertex(eyebrowR[i][0], eyebrowR[i][1]);
-  }
-  endShape();
 }
